dd58f1c862ffc61cf32c36403e58fdb3839b6e7f
Oteri Eyenike <teyenike1@gmail.com>
2025-02-23 14:57:13 +0100

    feat: add site title, tagline, description, and favicon

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Devolio';
-export const SITE_TAGLINE = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site tagline
-export const SITE_DESCRIPTION = 'Minimalist Starter for Developer Blog & Portfolio Website'; // site meta description for SEO
+export const SITE_TITLE = 'Teri';
+export const SITE_TAGLINE = 'Data meets development'; // site tagline
+export const SITE_DESCRIPTION = 'Simplifying data and empowering developers with insights'; // site meta description for SEO
