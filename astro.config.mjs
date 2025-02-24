555441993eef8abc5132b6c807ed2f7f9c678447
Oteri Eyenike <teyenike1@gmail.com>
2025-02-24 14:14:09 +0100

    feat: refactor the code

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://devolio.devaradise.com',
+  site: 'https://iamteri.tech',
   integrations: [mdx(), sitemap(), tailwind(), partytown()],
   markdown: {
     extendDefaultPlugins: true,
