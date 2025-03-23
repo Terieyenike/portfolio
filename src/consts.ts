6af56060a2c129d89c710318f55fc20bc9743da3
oteri <teyenike1@gmail.com>
2025-03-24 00:02:12 +0100

    feat: constant for meta tagline and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Teri';
-export const SITE_TAGLINE = 'Data meets development'; // site tagline
-export const SITE_DESCRIPTION = 'Simplifying data and empowering developers with insights'; // site meta description for SEO
+export const SITE_TITLE = "Teri";
+export const SITE_TAGLINE = "Software and DevOps Engineer"; // site tagline
+export const SITE_DESCRIPTION =
+  "Transforming complex technical challenges into elegant, scalable solutions";
