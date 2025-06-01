7151ea3de921be6564c8a99446af57fd397094af
oteri <teyenike1@gmail.com>
2025-06-01 16:56:28 +0100

    update: title

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Teri Eyenike";
-export const SITE_TAGLINE = "Creative Technologist and Educator | Building future tools"; 
+export const SITE_TAGLINE = "Creative Technologist & Educator | Building future tools"; 
 export const SITE_DESCRIPTION =
   "Combining software engineering and education to build interactive tools, creative experiences, and engaging ways to learn through tech";
